230b4c65f230a5a2b2e657ba22692ec4396a755d
Lemon Nyzer <lemonyzer@gmail.com>
2021-01-14 12:14:39 +0100

    using render_templates with basic static html

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, escape, request, render_template
+
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
-
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
